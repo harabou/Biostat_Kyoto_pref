e6ce93fdf0d48b0ed4a654bcea42a18940389e10
harabou <139622771+harabou@users.noreply.github.com>
2023-08-27 17:46:32 +0900

    Add files via upload

--- R_code/#03_Compare.R
+++ R_code/#03_Compare.R
@@ -5,6 +5,11 @@ ae <- read_sav("F:/¡¡¡‘åŠw/Ž ‰êˆã‘å/¡¡u‹`ŠÖ˜A/‹ž“s•{‘å/u‹`Ž‘—¿/2023_R
 
 ae2 <- read_sav("F:/¡¡¡‘åŠw/Ž ‰êˆã‘å/¡¡u‹`ŠÖ˜A/‹ž“s•{‘å/u‹`Ž‘—¿/2023_R/##‘æ3‰ñ/Compare/ae2.sav")
 
+#Github‚©‚ç
+ae <- read.csv("https://raw.githubusercontent.com/harabou/Biostat_Kyoto_pref/main/data/%2303/compare/ae.csv")
+ae2 <- read.csv("https://raw.githubusercontent.com/harabou/Biostat_Kyoto_pref/main/data/%2303/compare/ae2.csv")
+
+
 install.packages("arsenal")
 library(arsenal)
 
